14ef8d8670b9e58d15840e1d559068353f9a0a38
ryanbevins <ryanabevins@gmail.com>
2020-08-24 05:45:19 -0700

    Init

---
+++ KarKBD/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KarKBD
+{
+    internal class Program
+    {
+        private static string _pathToFile;
+        private static List<KBDButton> _buttons;
+        private static List<Tuple<int, int>> _positions;
+        private static byte[] _bytes;
+
+        // position, overflow/track value
+        private static List<int> bytePositions;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            OpenFile();
+            bytePositions = new List<int>();
+            _buttons = GetNoteButtons();
+            _positions = GetNotePositions();
+
+            Console.WriteLine("Note count: {0}", _buttons.Count);
+            for (var i = 0; i < _buttons.Count; i++)
+                switch (_buttons[i])
+                {
+                    case KBDButton.A:
+                        Console.WriteLine("X/A POSITION: {0} OVERFLOW: {1} | LOCATION: {2}", _positions[i].Item1,
+                            _positions[i].Item2, ToHex(bytePositions[i]));
+                        break;
+                    case KBDButton.B:
+                        Console.WriteLine("O/B: {0} OVERFLOW: {1} | LOCATION: {2}", _positions[i].Item1,
+                            _positions[i].Item2, ToHex(bytePositions[i]));
+                        break;
+                    case KBDButton.X:
+                        Console.WriteLine("SQUARE/X: {0} OVERFLOW: {1} | LOCATION: {2}", _positions[i].Item1,
+                            _positions[i].Item2, ToHex(bytePositions[i]));
+                        break;
+                    case KBDButton.Y:
+                        Console.WriteLine("TRIANGLE/Y: {0} OVERFLOW: {1} | LOCATION: {2}", _positions[i].Item1,
+                            _positions[i].Item2, ToHex(bytePositions[i]));
+                        break;
+                }
+
+            GetNoteButtons();
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+        private static void OpenFile()
+        {
+            var dialogBox = new OpenFileDialog();
+            dialogBox.Title = "Open karaoke bin file";
+            dialogBox.Filter = "Dragon Engine Karaoke .kbd|*.kbd";
+            dialogBox.InitialDirectory = @"C:\";
+            if (dialogBox.ShowDialog() == DialogResult.OK)
+            {
+                _pathToFile = dialogBox.FileName;
+                Console.WriteLine(dialogBox.FileName);
+            }
+            else
+            {
+                Console.Error.WriteLine("ERROR 00: Could not find file");
+            }
+
+            if (File.Exists(_pathToFile))
+            {
+                var reader = new BinaryReader(new FileStream(_pathToFile, FileMode.Open, FileAccess.ReadWrite));
+                reader.BaseStream.Position = 0x00;
+                _bytes = reader.ReadBytes((int) reader.BaseStream.Length);
+                reader.Close();
+            }
+        }
+
+
+        private static List<KBDButton> GetNoteButtons()
+        {
+            var buttonList = new List<KBDButton>();
+            var startingPos = 0x2C;
+            var jumpLength = 0x20;
+            for (var i = startingPos; i < _bytes.Length; i += jumpLength) buttonList.Add(FindButtonType(_bytes[i]));
+            return buttonList;
+        }
+
+        private static List<Tuple<int, int>> GetNotePositions()
+        {
+            var positionList = new List<Tuple<int, int>>();
+            var startingPos = 0x1D;
+            var jumpLength = 0x20;
+            for (var i = startingPos; i < _bytes.Length; i += jumpLength)
+            {
+                bytePositions.Add(i);
+                positionList.Add(new Tuple<int, int>(_bytes[i], _bytes[i + 1]));
+            }
+
+            return positionList;
+        }
+
+        private static KBDButton FindButtonType(byte b)
+        {
+            switch (b)
+            {
+                case 0x00:
+                    return KBDButton.B;
+                case 0x01:
+                    return KBDButton.A;
+                case 0x02:
+                    return KBDButton.X;
+                case 0x03:
+                    return KBDButton.Y;
+                default:
+                    Console.Error.WriteLine("ERROR 01: COULD NOT FIND BUTTON TYPE. TYPE RETURNED = {0}", b);
+                    return KBDButton.NULL;
+            }
+        }
+
+        public static string ToHex(int value)
+        {
+            return string.Format("0x{0:X}", value);
+        }
+
+        private enum KBDButton : byte
+        {
+            B = 0x00,
+            A = 0x01,
+            X = 0x02,
+            Y = 0x03,
+            NULL = 0x04
+        }
+    }
+}
---
+++ KarKBD/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KarKBD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KarKBD")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("401704DB-1F63-41C0-ACB1-1C27E4DC7605")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
